59eb6b4b8547da27efe470d83f5df2052a32f758
Conwa <conradodelcarlo97@gmail.com>
2023-05-25 17:02:35 -0300

    arregle el tema del offset

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const navLi = document.querySelectorAll(
   "ul.navbar-nav.flex-row.gap-2 li a.nav-link"
 );
 
-console.log(navLi);
-
 window.onscroll = () => {
   var current = "";
 
@@ -20,7 +18,7 @@ window.onscroll = () => {
   navLi.forEach((a) => {
     a.classList.remove("active");
     const href = a.getAttribute("href");
-    console.log(href);
+
     if (href === `#${current}`) {
       a.classList.add("active");
     }
